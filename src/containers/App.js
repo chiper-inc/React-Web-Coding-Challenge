4405b76c5d8b1ac49a856a343d428d4959f05db8
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2020-08-21 18:52:52 -0500

    Configuración inicial del proyecto

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Iniciando App React!</h1>
+      <Button variant="contained" color="primary">
+        Hola Mundo!
+      </Button>
+    </div>
+  )
+}
