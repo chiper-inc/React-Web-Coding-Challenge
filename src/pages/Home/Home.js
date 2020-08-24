e96de55c59c10b82e068fd749902e96060ec5736
Beto <betotoro0902@gmail.com>
2020-08-24 14:52:49 -0500

    [UPDATE] Home page src

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, H1, P } from './styles';
+import GlobalStyles from '../../GlobalStyles';
+import ListOfCards from '../../components/ListOfBikes/ListOfCards';
+
+const Home = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Container>
+        <Link to='/'>
+          <H1>Police Department of Berlin</H1>
+        </Link>
+        <P>Stolen Bikes</P>
+        <ListOfCards />
+      </Container>
+    </>
+  );
+};
+
+export default Home;
