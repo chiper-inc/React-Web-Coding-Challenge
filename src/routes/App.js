941edbb85c9aa512617bc6dbbe9c427a95bec099
Beto <betotoro0902@gmail.com>
2020-08-24 15:20:44 -0500

    [FIX] Config Router

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 
+const basename = process.env.NODE_ENV === 'development' ? '/' : '/React-Web-Coding-Challenge/';
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <Switch>
       <Route path='/' component={Home} />
     </Switch>
