58cb1742fd84b109a00bfc499911efcb6b2189fa
Juan Sarmiento <juandsarmiento@gmail.com>
2020-08-24 11:08:13 -0500

    fixing styles of HeaderMenu

--- src/components/HeaderMenu/styles.js
+++ src/components/HeaderMenu/styles.js
@@ -1,18 +1,17 @@
-import styled from 'styled-components/macro';
-
+import styled from 'styled-components'
 
 export const Container = styled.div`
-    display:flex;
-    width:100%;
-    overflow:hidden;
-    padding: 0.5rem 1rem;
-`;
+  display: flex;
+  width: 100%;
+  overflow: hidden;
+  padding: 0.5rem 1rem;
+`
 
 export const Img = styled.img`
-    height:100px;    
-`;
+  height: 100px;
+`
 export const ContainerTitles = styled.div`
-    margin-left:0.5rem;
-`;
-export const Title = styled.h3``;
-export const SubTitle = styled.h6``;
+  margin-left: 0.5rem;
+`
+export const Title = styled.h3``
+export const SubTitle = styled.h5``
