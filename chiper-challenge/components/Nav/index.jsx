51c88ee88f7d95510f69fce7b541c2d0775764f5
Mariano Baeza <marianobaeza@hotmail.com>
2021-07-29 12:37:57 -0300

    adds basic nav with logo

---
+++ chiper-challenge/components/Nav/index.jsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+const MainContainer = styled.div`
+    width: %100;
+    background-color: #191919;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+const LogoContainer = styled.div`
+    display: flex;
+    align-items: center;
+`
+const Logo = styled.img`
+    height: 40px; 
+    margin: 10px;
+`
+const Title = styled.span`
+    font-size: 30px;
+    color: white;
+`
+
+const Nav = () => {
+    return (
+        <MainContainer>
+            <LogoContainer>
+                <Logo src='./img/logo.png' />
+                <Title>Police Departament of Berlin</Title>
+            </LogoContainer>
+        </MainContainer>
+    )
+}
+
+export default Nav
