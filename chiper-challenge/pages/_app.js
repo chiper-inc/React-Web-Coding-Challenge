533c1ba3a8a2067121e1e0672abafac468b69423
Mariano Baeza <marianobaeza@hotmail.com>
2021-08-02 13:00:19 -0300

    adds favicon

--- chiper-challenge/pages/_app.js
+++ chiper-challenge/pages/_app.js
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 import Nav from '../components/Nav'
+import Head from 'next/head'
 import { Provider } from 'react-redux'
 import store from '../store'
 import Footer from '../components/Footer'
 
-function MyApp ({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store} >
+      <Head>
+        <link rel="shortcut icon" href="/img/logo.png" />
+      </Head>
       <Nav />
       <Component {...pageProps} />
       <Footer />
