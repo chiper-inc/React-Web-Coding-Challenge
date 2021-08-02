d8f33847df1d312397ffff45d75d27ca5929a935
Mariano Baeza <marianobaeza@hotmail.com>
2021-08-01 23:17:58 -0300

    adds footer

---
+++ chiper-challenge/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const MainContainer = styled.div`
+    height: 15rem;
+    width: 100%;
+    background-color: #191919;
+    margin-top: 40px;
+`
+
+const Footer = () => {
+    return (
+        <MainContainer />
+    )
+}
+
+export default Footer
--- chiper-challenge/pages/_app.js
+++ chiper-challenge/pages/_app.js
@@ -2,12 +2,14 @@ import '../styles/globals.css'
 import Nav from '../components/Nav'
 import { Provider } from 'react-redux'
 import store from '../store'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store} >
       <Nav />
       <Component {...pageProps} />
+      <Footer />
     </Provider>
   )
 }
