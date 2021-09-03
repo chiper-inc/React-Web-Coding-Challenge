52770ddb10e7cc616c3a32e6214b3557b4c64f69
Sebastian <tassone_af@yahoo.com>
2021-09-03 20:29:36 -0300

    feat: add props to card

--- stolen-bikes-index-app/src/components/Card/Card.tsx
+++ stolen-bikes-index-app/src/components/Card/Card.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { BikeCard } from '../../interfaces/BikeCardInterface';
 import './Card.css';
 
-export default function Card() {
+export default function Card({ title, description, img } :BikeCard) {
   return (
-    <div />
+    <div>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h2>{description}</h2>
+    </div>
   );
 }
