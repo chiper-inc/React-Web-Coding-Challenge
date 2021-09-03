39d33a4bd1f66ca5c93cfa499f2a9486860203c0
Sebastian <tassone_af@yahoo.com>
2021-09-03 11:27:59 -0300

    feat: add routes

--- stolen-bikes-index-app/src/App.tsx
+++ stolen-bikes-index-app/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Nav from './components/Nav/Nav';
+import StolenBike from './components/StolenBike/StolenBike';
 
 function App() {
   return (
     <div className="App">
-      aasd
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route path="/bike/:id" component={StolenBike} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- stolen-bikes-index-app/src/components/StolenBike/StolenBike.tsx
+++ stolen-bikes-index-app/src/components/StolenBike/StolenBike.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function StolenBike() {
   return (
     <div>
-      home
+      det
     </div>
   );
 }
