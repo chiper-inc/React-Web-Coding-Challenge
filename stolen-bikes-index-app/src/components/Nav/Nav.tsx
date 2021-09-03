d5e62845ca0b139e7d13cd97c4ea92cdd6036935
Sebastian <tassone_af@yahoo.com>
2021-09-03 11:40:25 -0300

    feat: nav function

--- stolen-bikes-index-app/src/components/Nav/Nav.tsx
+++ stolen-bikes-index-app/src/components/Nav/Nav.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Nav.css';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/Berliner_Polizei.svg.png';
 
 export default function Nav() {
   return (
-    <div>
-      nav
+    <div className="nav">
+      <Link to="/">
+        <img src={logo} alt="" />
+      </Link>
     </div>
   );
 }
