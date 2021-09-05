57e2f8ed604cfc76cb6e45ce1f8ebc052f747e6b
Sebastian <tassone_af@yahoo.com>
2021-09-05 16:52:33 -0300

    fix: interfaces request

--- stolen-bikes-index-app/src/components/Home/Home.tsx
+++ stolen-bikes-index-app/src/components/Home/Home.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { SimpleStolenBikes } from '../../interfaces/SimpleStolenBikesInterface';
 import { getStolenBikes } from '../../redux/actions';
 import { State } from '../../redux/reducer';
 import DisplayCards from '../DisplayCards/DisplayCards';
@@ -10,15 +11,18 @@ export default function Home() {
   const dispatch = useDispatch();
   const allItems = useSelector((state:State) => state.stolenBikes);
  
+  const [filteredResults, setFilteredResults] = useState<SimpleStolenBikes[]>([]);
+
   useEffect(() => {
     if (!allItems.length) { dispatch(getStolenBikes()); }
+    setFilteredResults([]);
   }, []);
 
   return (
     <div className="homeCtn">
       <h1>Stolen bikes</h1>
       <SearchBar />
-      <DisplayCards list={allItems} />
+      <DisplayCards list={filteredResults.length ? filteredResults : allItems} />
     </div>
   );
 }
--- stolen-bikes-index-app/src/redux/actions/index.ts
+++ stolen-bikes-index-app/src/redux/actions/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import BikeIndexApi from '../../api/BikeIndexApi';
 import { SimpleStolenBikes } from '../../interfaces/SimpleStolenBikesInterface';
 import { StolenBikes } from '../../interfaces/StolenBikesInterface';
@@ -12,17 +13,18 @@ export const GET_STOLEN_BIKES = 'GET_STOLEN_BIKES';
 export const getStolenBikes = () => function (dispatch: any) {
   return BikeIndexApi.get<StolenBikes>('/search?page=1&per_page=100&location=Berlin&distance=10&stolenness=proximity')
     .then((response) => {
+      console.log(response.data.bikes);
       dispatch({
         type: GET_STOLEN_BIKES,
         payload: response.data.bikes.map(({
-          id, title, description, dateStolen, stolenLocation, largeImg,
+          id, title, description, date_stolen, stolen_location, large_img,
         }):SimpleStolenBikes => ({
           id,
           title,
-          dateOfTheft: (new Date(dateStolen * 1000)).toDateString(),
+          dateOfTheft: (new Date(date_stolen * 1000)).toDateString(),
           description: `${description?.substr(0, 100)}...`,
-          locationOfTheft: stolenLocation,
-          img: largeImg,
+          locationOfTheft: stolen_location,
+          img: large_img,
         })),
       });
     });
