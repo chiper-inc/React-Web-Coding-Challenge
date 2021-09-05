830bee4a78b1ff99844cb5cadb4660ca5617a0d6
Sebastian <tassone_af@yahoo.com>
2021-09-04 21:46:20 -0300

    feat: maping cards

--- stolen-bikes-index-app/src/components/Card/Card.tsx
+++ stolen-bikes-index-app/src/components/Card/Card.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { SimpleStolenBikes } from '../../interfaces/SimpleStolenBikesInterface';
 import './Card.css';
 
-export default function Card({ title, description, img } :SimpleStolenBikes) {
+export default function Card({
+  id, dateOfTheft, img, description, locationOfTheft, title,
+}:SimpleStolenBikes) {
   return (
     <div>
       <img src={img || ''} alt="" />
-      <h1>{title}</h1>
-      <h2>{description}</h2>
+      <span>{title}</span>
+      <span>{description}</span>
+      <span>{locationOfTheft}</span>
+      <span>{dateOfTheft}</span>
+      <span>{id}</span>
     </div>
   );
 }
--- stolen-bikes-index-app/src/components/Home/Home.tsx
+++ stolen-bikes-index-app/src/components/Home/Home.tsx
@@ -1,20 +1,33 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import usePagination from '../../hooks/usePagination';
 import { getStolenBikes } from '../../redux/actions';
 import { State } from '../../redux/reducer';
+import Card from '../Card/Card';
 import './Home.css';
 
 export default function Home() {
-  const a = useSelector((state:State) => state);
-  const d = useDispatch();
+  const bikes = useSelector((state:State) => state.stolenBikes);
+  const dispatch = useDispatch();
+
+  const { page } = usePagination(bikes);
 
   useEffect(() => {
-    d(getStolenBikes());
+    dispatch(getStolenBikes());
   }, []);
 
   return (
     <div className="homeCtn">
       <h1>Stolen bikes</h1>
+      {page.map((element) => (
+        <Card
+          id={element.id}
+          title={element.title}
+          dateOfTheft={element.dateOfTheft}
+          description={element.description}
+          locationOfTheft={element.locationOfTheft}
+        />
+      ))}
     </div>
   );
 }
