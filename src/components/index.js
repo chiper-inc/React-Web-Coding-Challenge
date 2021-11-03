bca6c7fb94c9168212079929f4c215ec4e2f08bf
Ing Emanuel Torres <emanueljtc@gmail.com>
2021-11-02 20:41:54 -0400

    add components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import BarFilter from './BarFilter';
 import Header from './Header';
+import BikeItem from './BikeItem';
+import Pagination from './Pagination';
 
-export { Header, BarFilter };
+export { Header, BarFilter, BikeItem, Pagination };
