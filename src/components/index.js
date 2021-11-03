217da5d208ed9b8f590b46ea24b2932040d77269
Ing Emanuel Torres <emanueljtc@gmail.com>
2021-11-03 08:02:27 -0400

    component error

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Header from './Header';
 import BikeItem from './BikeItem';
 import Pagination from './Pagination';
 import Loading from './Loading';
+import ErrorData from './Error';
 
-export { Header, BarFilter, BikeItem, Pagination, Loading };
+export { Header, BarFilter, BikeItem, Pagination, Loading, ErrorData };
