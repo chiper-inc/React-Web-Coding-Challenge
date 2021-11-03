2407fdfa9d70f8d99d847f14350b17038a1979c8
Ing Emanuel Torres <emanueljtc@gmail.com>
2021-11-03 11:57:18 -0400

    any sections

--- src/views/details/index.tsx
+++ src/views/details/index.tsx
@@ -64,20 +64,109 @@ const Details = () => {
       ) : showError ? (
         <ErrorData />
       ) : (
-        <section className="detail-bike">
-          <p className="stolen">stolen</p>
-          <h1 className="title">
-            {' '}
-            {info?.year} <span>{info?.manufacturer_name}</span>{' '}
-            {info?.frame_model}
-          </h1>
-          <p className="date">
-            <span>Stolen </span>
-            {dateTransform}
-            <span> from </span>
-            {info?.stolen_location}
-          </p>
-        </section>
+        <div className="detail-bike">
+          <section className="header-detail flex flex-col">
+            <p className="stolen">stolen</p>
+            <h1 className="title">
+              {' '}
+              {info?.year} <span>{info?.manufacturer_name}</span>{' '}
+              {info?.frame_model}
+            </h1>
+            <p className="date">
+              <span>Stolen </span>
+              {dateTransform}
+              <span> from </span>
+              {info?.stolen_location}
+            </p>
+          </section>
+
+          <section className="details">
+            <div className="content-img w-full md:w-2/4">
+              <img
+                src={info?.large_img}
+                alt={info?.title}
+                className="img-bike"
+              />
+            </div>
+
+            <div className="info-details w-full md:w-2/4">
+              <div className="col-1">
+                <p>
+                  <span>Serial:</span>{' '}
+                  {!!info?.serial ? info?.serial : 'No information'}
+                </p>
+                <p>
+                  <span>Model:</span>{' '}
+                  {!!info?.frame_model ? info?.frame_model : 'No information'}
+                </p>
+                <p>
+                  <span>Primary colors:</span>{' '}
+                  {!!info?.frame_colors
+                    ? info?.frame_colors.map((color) => `${color} `)
+                    : 'No information'}
+                </p>
+                <p>
+                  <span>Type:</span>{' '}
+                  {!!info?.type_of_cycle
+                    ? info?.type_of_cycle
+                    : 'No information'}
+                </p>
+              </div>
+              <div className="col-2">
+                <p>
+                  <span>Manufacturer:</span>{' '}
+                  {!!info?.manufacturer_name
+                    ? info?.manufacturer_name
+                    : 'No information'}
+                </p>
+                <p>
+                  <span>Year:</span>{' '}
+                  {!!info?.year ? info?.year : 'No information'}
+                </p>
+                <p>
+                  <span>Frame size:</span>{' '}
+                  {!!info?.frame_size ? info?.frame_size : 'No information'}
+                </p>
+                <p>
+                  <span>Frame Material:</span>{' '}
+                  {!!info?.frame_material_slug
+                    ? info?.frame_material_slug
+                    : 'No information'}
+                </p>
+              </div>
+            </div>
+          </section>
+
+          <section className="distinguishing-features mt-4 w-full md:w-2/4">
+            <h3 className="title-detail">distinguishing features</h3>
+            <p>{!!info?.description ? info?.description : 'No description.'}</p>
+          </section>
+
+          <section className="theft-details mt-4 w-full md:w-2/4">
+            <h3 className="title-detail">theft details</h3>
+          </section>
+          <section className="description-of-incident mt-4 w-full md:w-2/4">
+            <h3 className="title-detail">description of incident</h3>
+            <p>
+              {!!info?.stolen_record?.theft_description
+                ? info?.stolen_record?.theft_description
+                : 'No description.'}
+            </p>
+          </section>
+          <section className="additional-parts mt-4 w-full md:w-2/4">
+            <h3 className="title-detail">additional parts</h3>
+            {!!info.components
+              ? info.components.map(({ component_type, description }) => {
+                  return (
+                    <p>
+                      <span>{!!component_type ? component_type : 'n/a'}</span>
+                      {!!description ? description : 'n/a'}
+                    </p>
+                  );
+                })
+              : 'No components'}
+          </section>
+        </div>
       )}
     </div>
   );
