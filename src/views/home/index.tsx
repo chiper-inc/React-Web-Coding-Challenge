cbca5d2a897dfc749fc05ce2fee68b72029bbe8d
Ing Emanuel Torres <emanueljtc@gmail.com>
2021-11-02 20:43:06 -0400

    add pagination

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react';
-import { BarFilter, Header } from '../../components';
-import BikeItem from '../../components/BikeItem';
+import { BarFilter, Header, BikeItem, Pagination } from '../../components';
 import BikesServices from '../../utils/services/BikesServices';
 
 const Home = () => {
   const [bikes, setBikes] = useState([]);
+  const [page, setPage] = useState(1);
+  /* Aca se hace el calculo de total de paginas el 61 fue extraido de la documentacion
+    de la API por alguna razon este valor no lo retornan en la data y por eso no se puede 
+    saber dinamicamente el valor y se divide entre 9 para que me de un valor flotante mayor a 
+    6.5 y esto de 7 que es el total de paginas filtradas por 10 items*/
+  const totalPage = Math.round(61 / 9);
 
   const searchAll = async () => {
     try {
-      const bikesResponse = await BikesServices.index();
+      const bikesResponse = await BikesServices.index(page);
       // console.log(bikesResponse.bikes);
       setBikes(bikesResponse.bikes);
     } catch (err) {
@@ -20,6 +25,10 @@ const Home = () => {
     searchAll();
   }, []);
 
+  useEffect(() => {
+    searchAll();
+  }, [page]);
+
   return (
     <div className="home">
       <Header />
@@ -27,6 +36,7 @@ const Home = () => {
       {bikes.map((bike) => {
         return <BikeItem bike={bike} />;
       })}
+      <Pagination page={page} setPage={setPage} totalPage={totalPage} />
     </div>
   );
 };
