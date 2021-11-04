b783cc7160e2923b88755b1717fe5b3ca003a2ec
Ing Emanuel Torres <emanueljtc@gmail.com>
2021-11-04 08:04:17 -0400

    change fn

--- src/tests/BarFilter.test.tsx
+++ src/tests/BarFilter.test.tsx
@@ -3,8 +3,10 @@ import { shallow } from 'enzyme';
 import BarFilter from '../components/BarFilter';
 
 test('renders the component BarFilter Test', () => {
+  const setTitleSearch = jest.fn();
+
   const component = shallow(
-    <BarFilter titleSearch="" setTitleSearch={() => console.log('')} />
+    <BarFilter titleSearch="" setTitleSearch={() => setTitleSearch()} />
   );
   expect(component).toMatchSnapshot();
 });
