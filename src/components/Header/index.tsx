b967e972edd4b313c79238a0e37e62fd820d2395
Ing Emanuel Torres <emanueljtc@gmail.com>
2021-11-04 08:56:18 -0400

    change Link to A

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,9 +9,9 @@ const Header = () => {
         <img src={Logo} alt="Logo" />
       </div>
       <div className="content-info">
-        <Link className="title" to="/">
+        <a className="title" href="/">
           Police Department of Berlin
-        </Link>
+        </a>
         <h4 className="sub-title">Stolen bykes.</h4>
       </div>
     </div>
