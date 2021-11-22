31889f7799da226ceb89c0cbf5e2904cdca6ce15
Carlos Abreu <parakletos75@gmail.com>
2021-11-22 18:13:17 -0400

    fix map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -2,9 +2,14 @@ import { Map, GoogleApiWrapper } from "google-maps-react";
 
 const Maps = (props) => {
   const { google } = props;
+  const containerStyle = {
+    width: '80%',
+    height: '70%'
+  };
   return (
     <>
       <Map
+        containerStyle={containerStyle}
         google={google}
         zoom={13}
         initialCenter={{ lat: 52.5229397, lng: 13.4146747 }}
