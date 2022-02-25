d6ddb6b12065882efe23ea8225d753e7d3155bb6
Ivan Diaz <ivancho2802@hotmail.com>
2022-02-24 19:48:05 -0500

    paginate

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ This app needs to display the list of reported bike thefts based on the Bikewise
 
 As a police officer:
 
-- [ ] I want to see a list of reported bike thefts for the Berlin area.
-- [ ] I want to see the first 10 bike theft cases, with the ability to - paginate (10 cases per page).
+- [x] I want to see a list of reported bike thefts for the Berlin area.
+- [x] I want to see the first 10 bike theft cases, with the ability to - paginate (10 cases per page).
 - [ ] I want to see a total number of bike theft cases.
 - [ ] For each reported bike theft I want to see:
   - [ ] Case title
--- src/App.js
+++ src/App.js
@@ -1,10 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg'
+import './App.css'
 import Head from 'next/head'
-
-//
-var bikes = [];
-//...Array(100).keys()
+import { React, useState, useEffect } from 'react'
+//import ReactDOM from 'react-dom'
+/* class Header extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+} */
+function BikeRobery() {
+  let [error, setError] = useState(null)
+  let [isLoaded, setIsLoaded] = useState(false)
+  let [bikes, setBikes] = useState([])
+  let [page, setPage] = useState(1)
+  //const [per_page, setPPage] = useState(10)
+  useEffect(() => {
+    fetch('https://bikeindex.org/api/v3/search?stolenness=proximity&location=Berlin')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true)
+          setBikes(result.bikes)
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true)
+          setError(error)
+        }
+      )
+  }, [])
+  if (error) {
+    return <div>Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className="grid">
+        {bikes.map((el) => (
+          <a href={el.url} className="card">
+            <img
+              src={
+                el.thumb
+                  ? el.thumb
+                  : 'https://bikeindex.org/assets/revised/bike_photo_placeholder-ff15adbd9bf89e10bf3cd2cd6c4e85e5d1056e50463ae722822493624db72e56.svg'
+              }
+            />
+            <div className="content">
+              <h3>{el.title} </h3>
+              <p>Serial: {el.serial} </p>
+              <p>
+                Primary colors:
+                {el.frame_colors.map((el2) => (
+                  <span>{el2}</span>
+                ))}
+              </p>
+              <p>
+                <span style={{ color: el.stolen ? 'red' : 'none' }}>STOLEN</span>:
+                {new Intl.DateTimeFormat('en-US').format(el.date_stolen)}
+              </p>
+              <p>{el.stolen_location}</p>
+            </div>
+          </a>
+        ))}
+        {page <= 1 ? (
+          <div className="grid">
+            <button onClick={() => setPage((page += 1))}>Next Page</button>
+          </div>
+        ) : page > 1 ? (
+          <div className="grid">
+            <button onClick={() => setPage((page -= 1))}>Back Page</button>
+          </div>
+        ) : null}
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
@@ -15,26 +87,15 @@ function App() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Police Departament of Berlin
-        </h1>
+        <h1 className="title">Police Departament of Berlin</h1>
 
-        <p className="description">
-        Stolen Bikes
-        </p>
+        <p className="description">Stolen Bikes</p>
 
-        <div className="grid">
-          
-          {
-            bikes.map(el => 
-            <a href="#" className="card">
-              <h3>{el} &rarr;</h3>
-              <p>{el}</p>
-            </a>)
-          }
+        <div>
+          <BikeRobery />
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
+          {/* <a href="https://nextjs.org/learn" className="card">
+            <h3>Learn </h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
@@ -42,7 +103,7 @@ function App() {
             href="https://github.com/vercel/next.js/tree/master/examples"
             className="card"
           >
-            <h3>Examples &rarr;</h3>
+            <h3>Examples </h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
@@ -50,11 +111,9 @@ function App() {
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <h3>Deploy </h3>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+          </a> */}
         </div>
       </main>
 
@@ -64,8 +123,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
@@ -80,7 +138,6 @@ function App() {
         }
 
         main {
-          padding: 5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
@@ -144,8 +201,8 @@ function App() {
           border-radius: 5px;
           padding: 0.75rem;
           font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+            Bitstream Vera Sans Mono, Courier New, monospace;
         }
 
         .grid {
@@ -160,7 +217,7 @@ function App() {
 
         .card {
           margin: 1rem;
-          flex-basis: 45%;
+          flex-basis: 100%;
           padding: 1.5rem;
           text-align: left;
           color: inherit;
@@ -168,6 +225,12 @@ function App() {
           border: 1px solid #eaeaea;
           border-radius: 10px;
           transition: color 0.15s ease, border-color 0.15s ease;
+        }
+        .card .content {
+          -webkit-box-flex: 0;
+          flex: 0 0 75%;
+          padding: 12px 0.9375rem;
+          float: left;
         }
 
         .card:hover,
@@ -187,7 +250,15 @@ function App() {
           font-size: 1.25rem;
           line-height: 1.5;
         }
-
+        .card img {
+          background: #f2f2f2;
+          height: 200px;
+          margin: auto;
+          min-width: 70px;
+          padding: 24px;
+          vertical-align: middle;
+          float: left;
+        }
         .logo {
           height: 1em;
         }
@@ -205,9 +276,8 @@ function App() {
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
         }
 
         * {
@@ -215,7 +285,7 @@ function App() {
         }
       `}</style>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
