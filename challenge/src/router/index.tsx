bd06571de581f0e7fff2b127f0007e9f1348754e
Jonás Dias <jonasuneg@gmail.com>
2022-03-12 19:15:05 -0500

    add not found page

--- challenge/src/router/index.tsx
+++ challenge/src/router/index.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import * as Routing from './routing';
 import Login from '../screens/login';
 import Dashboard from './dashboard';
+import NotFound from '../screens/404';
 
 const Router = () => {
   return (
     <Routes>
       <Route path={Routing.LoginRoute} element={<Login />} />
       <Route path={Routing.Dashboard} element={<Dashboard />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
