a5be460adbed63a8798737fbd3e008b4b6829ca8
Jose Manuel Rojas <jose.rojas@conekta.com>
2022-05-07 09:37:41 -0500

    adding header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,16 @@
+import './styles.scss';
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='Header'>
+      <img 
+      alt='berlin police' 
+      src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Coat_of_arms_of_Berlin.svg/180px-Coat_of_arms_of_Berlin.svg.png' />
+      <div>
+        <h1>Police Departament of Berlín</h1>
+        <h4>Stolen bykes</h4>
+      </div>
+    </header>
   )
 }
 
